740b4a80ad3b562b35bf37dee468336167eec307
OniiKuma <105169315+OniiKuma@users.noreply.github.com>
2022-10-20 01:18:55 -0700

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,10 @@
+//this sets and names the the function
+function myFunction() { //opening scope operator
+    //this sets our alert and names it 
+    alert('Started');
+} //closing scope operator
+//this sets and names the the function
+function myFunction2() { //opening scope operator
+    //this sets our alert and names it 
+    alert('Stopped');
+} //closing scope operator
